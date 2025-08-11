dfcd504405173f618f0ab0a2f9c9e6e865d2de6f
Joliph2427 <joliph01macwan@gmail.com>
2025-08-10 23:16:31 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -30,34 +30,61 @@ const app = Vue.createApp({
     methods: {
         User() {
             fetch('https://comp6062.liamstewart.ca/random-user-data')
-                .then(response => response.json())
+                .then(response => {
+                    if(response.ok)
+                    {
+                        return response.json();
+                    }
+                    else
+                    {
+                        console.log('an error occured.please try again');
+                    }
+                })
                 .then(data => {
                     this.user.profile_picture = data.user_profile.avatar_url;
                     this.user.first_name = data.user_profile.first_name;
                     this.user.last_name = data.user_profile.last_name;
                     this.user.age = data.user_profile.age;
                 })
-                .catch(error => console.error('User fetch error:', error));
+                .catch(error => {console.log('random user fetch error');});
         },
         Weather() {
             fetch(`https://comp6062.liamstewart.ca/weather-data?city=${this.weather.city}&province=${this.weather.province}&country=${this.weather.country}`)
-                .then(response => response.json())
+                .then(response =>  {
+                    if(response.ok)
+                    {
+                        return response.json();
+                    }
+                    else
+                    {
+                        console.log('an error occured.please try again');
+                    }
+                })
                 .then(data => {
                     this.weather.temp = data.weather_data.temperature;
                     this.weather.wind = data.weather_data.wind_speed;
                     this.weather.desc = data.weather_data.weather_description;
                 })
-                .catch(error => console.error('Weather fetch error:', error));
+                .catch(error => {console.log('Weather fetch error');});
         },
         Dictionary() {
             fetch(`https://comp6062.liamstewart.ca/api/define?word=${this.dictionary.word}`)
-                .then(response => response.json())
+                .then(response =>  {
+                    if(response.ok)
+                    {
+                        return response.json();
+                    }
+                    else
+                    {
+                        console.log('an error occured.please try again');
+                    }
+                })
                 .then(data => {
                     this.dictionary.word = data.word;
                     this.dictionary.phonetic = data.phonetic;
                     this.dictionary.definition = data.definition;
                 })
-                .catch(error => console.error('Dictionary fetch error:', error));
+                .catch(error => {console.log('Dictionary fetch error');});
         }
     }
 });
